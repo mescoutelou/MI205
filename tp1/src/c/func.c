d9ec13c9561ddd6fefc18a7c47246abfa227a8fb
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-11-07 14:49:30 +0100

    Updates to support Newlib RISC-V toolchain

--- tp1/src/c/func.c
+++ tp1/src/c/func.c
@@ -3,7 +3,7 @@
  * Created Date: 2023-10-09 08:59:17 pm
  * Author: Mathieu Escouteloup
  * -----
- * Last Modified: 2023-10-11 08:00:29 am
+ * Last Modified: 2023-11-07 02:47:05 pm
  * Modified By: Mathieu Escouteloup
  * -----
  * License: See LICENSE.md
@@ -14,22 +14,22 @@
 
 #include "func.h"
 
-void func_set_pin_c (int *p_gpio, char pin) {
+void func_set_pin_c (uint32_t *p_gpio, uint8_t pin) {
   
 }
 
-void func_reset_pin_c (int *p_gpio, char pin) {
+void func_reset_pin_c (uint32_t *p_gpio, uint8_t pin) {
   
 }
 
-short func_mul_c (char a, char b) {
+uint16_t func_mul_c (uint8_t a, uint8_t b) {
   return 0;
 }
 
-short func_mulop_c (char a, char b) {
+uint16_t func_mulop_c (uint8_t a, uint8_t b) {
   return 0;
 }
 
-short func_mulp2_c (char a, char b) {
+uint16_t func_mulp2_c (uint8_t a, uint8_t b) {
   return 0;
 }
